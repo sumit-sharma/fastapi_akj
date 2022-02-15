6b99bdebfaacc642e6396c6792770681f1770977
sumit <211.sumit@gmail.com>
2022-02-15 18:48:17 +0530

    mysql error fixes

--- app/database.py
+++ app/database.py
@@ -6,7 +6,10 @@ from settings import dbusername, dbpassword, dbname, dbhost
 # SQL_ALCHEMY_DB_URL = 'sqlite:///./akj.db'
 # SQL_ALCHEMY_DB_URL = 'mysql://root:root@localhost/app_jyotish_db'
 
-SQL_ALCHEMY_DB_URL = 'mysql://'+dbusername+':'+dbpassword+'@'+dbhost+'/'+dbname
+# SQL_ALCHEMY_DB_URL = 'mysql://'+dbusername+':'+dbpassword+'@'+dbhost+'/'+dbname
+
+SQL_ALCHEMY_DB_URL = 'mysql://deluser:Sysadmin#$987@13.233.156.171/akj_db_user'
+
 engine = create_engine(SQL_ALCHEMY_DB_URL, encoding='latin1', echo=True)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,) 
