24ec49f30a8c3373e22eea17c67fbe76b405e2fd
sumit <211.sumit@gmail.com>
2022-03-08 21:00:08 +0530

    auth chnages

--- app/api/api_v1/endpoints/auth.py
+++ app/api/api_v1/endpoints/auth.py
@@ -8,14 +8,47 @@ import database, models
 from schema.auth import AuthModel, LoginModel
 from fastapi.responses import JSONResponse
 from schema.user import UserModel
+import requests
+
 router = APIRouter()
 
 get_db = database.get_db
 
 
-def sendsms(country_code, mobile):
-    pass
+# function to generate OTP
+def generateOTP() :
+ 
+    # Declare a digits variable 
+    # which stores all digits
+    digits = "0123456789"
+    OTP = ""
+ 
+   # length of password can be changed
+   # by changing value in range
+    for i in range(4) :
+        OTP += digits[math.floor(random.random() * 10)]
+ 
+    return OTP
 
+def sendsms(country_code, mobile):
+    
+	# api-endpoint
+	# URL = "http://164.52.195.161/API/SendMsg.aspx"
+	URL =  "http://164.52.195.161/API/BalAlert.aspx"
+	# location given here
+	
+	  
+	# defining a params dict for the parameters to be sent to the API
+	# PARAMS = {'dest': mobile, 'msg': "Dear%20Customer,%0A%0AOTP%20for%20verify%20your%20mobile%20number%20on%20%20test%20is%20test.%0AKANNDS"}
+	PARAMS = {'uname': 20220081, 'pass': "G99x9GJX", 'send': "KANNDS"}
+	  
+	# sending get request and saving the response as response object
+	r = requests.get(url = URL, params = PARAMS)
+	  
+	# extracting data in json format
+	data = r.json()
+	
+	return data
 
 def check_user(country_code, mobile, db: Session = Depends(get_db)):
     status = False
@@ -28,15 +61,17 @@ def check_user(country_code, mobile, db: Session = Depends(get_db)):
 
 @router.post("/send_otp")
 def send_otp(item: AuthModel, db: Session = Depends(get_db)):
-    user = check_user(item.country_code, item.mobile, db)
-    if(user):
-        otp =  models.AccountOtp(type="mobile", otp="1234", user_id = user.id, reference= "login")
-        db.add(otp)
-        db.commit()
-        db.refresh(otp)
-        return otp
-    else:
-        return JSONResponse(status_code=404, content={"message": "user not found"})
+    return sendsms(item.country_code, item.mobile)
+    
+    # user = check_user(item.country_code, item.mobile, db)
+    # if(user):
+    #     otp =  models.AccountOtp(type="mobile", otp=generateOTP(), user_id = user.id, reference= "login")
+    #     db.add(otp)
+    #     db.commit()
+    #     db.refresh(otp)
+    #     return otp
+    # else:
+    #     return JSONResponse(status_code=404, content={"message": "user not found"})
 
 
 
